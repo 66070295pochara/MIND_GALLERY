9ee3da185868ccf83460ae145c1ca58e1128dfdb
ThanakritSt <141338370+Thanakrit66070262@users.noreply.github.com>
2025-10-01 02:21:39 +0700

    S3 owngallery
    personal s3 upload folder

---
+++ routes/view.routes.js
@@ -0,0 +1,17 @@
+
+const router = require("express").Router();
+const { requireAuth } = require("../middlewares/authenticate");
+const { renderOwnGallery } = require("../controllers/gallery.controller.js");
+
+// แก้บั๊กเดิมที่ใช้ res.redirect, { ... }
+router.get("/", (req, res) => res.redirect("/login"));
+
+router.get("/upload", requireAuth, (req, res) => {
+  const claims = req.user || {};
+  const username = req.query.username || claims['cognito:username'] || claims.email || "Anonymous";
+  res.render("upload", { title: "Mind Gallery - Upload", username });
+});
+
+router.get("/owngallery", requireAuth, renderOwnGallery);
+
+module.exports = router;
