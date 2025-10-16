7376bb24f68543f0bc24285da7feedbf51ac21ad
ThanakritSt <141338370+Thanakrit66070262@users.noreply.github.com>
2025-10-16 19:19:06 +0700

    Merge branch 'main' of https://github.com/66070295pochara/MIND_GALLERY

--- routes/upload.routes.js
+++
@@ -1,9 +0,0 @@
-// routes/upload.routes.js
-const router = require('express').Router();
-const { requireAuth } = require('../middlewares/authenticate');
-const uploadController = require('../controllers/upload.controller');
-
-router.put('/presign', requireAuth, uploadController.getPresignedPutUrl);
-router.get('/', uploadController.renderUploadPage);
-
-module.exports = router;
